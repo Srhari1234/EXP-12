b7cdf5bd9042914797f0eaebc256392738941d50
Srihari Nair <sriharinair134@gmail.com>
2024-09-03 19:55:20 +0530

    Add files via upload

---
+++ 12_4 Parameterized constructor.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Num
+{
+    public:
+    Num(int c, int d)
+    {
+        if(c>d)
+        {
+            cout<<c<<" is greater";
+        }
+        else
+        {
+            cout<<d<<" is greater";
+        }
+    }
+};
+int main()
+{
+Num n1(4,3);
+}
